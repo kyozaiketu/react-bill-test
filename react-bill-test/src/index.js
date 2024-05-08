01b84c55933e9e651144956f2343d1a7b26cf717
kyozaiketu <kyozkt_186@outlook.jp>
2024-05-08 09:19:09 +0900

    master

---
+++ react-bill-test/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import sum from '@/test';
+
+const total = sum(1, 3)
+console.log(total);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <App />
+)
+
